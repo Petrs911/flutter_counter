5695f847084a6670cabaf8cbf9b998f3c6615298
Kyrylo Petrenko <petrs911@gmail.com>
2025-11-05 15:10:47 +0200

    Add Fastlane setup for iOS

--- fastlane/Fasthelper.rb
+++ fastlane/Fasthelper.rb
@@ -1,25 +1,22 @@
 module SharedFastlane
+  def self.flutter_root(fastfile_dir:)
+    File.expand_path('../..', fastfile_dir)
+  end
+
+  private_class_method :flutter_root
+
+  def self.service_account_path(file_name:, fastfile_dir:)
+    root = flutter_root(fastfile_dir: fastfile_dir)
+    File.join(root, 'fastlane', 'service_account', file_name)
+  end
+
   def self.validate_file_exists(file_path:, file_name:)
     FastlaneCore::UI.user_error!("❌ #{file_name} not found at: #{file_path}") unless File.exist?(file_path)
     FastlaneCore::UI.message("✅ #{file_name} found")
   end
 
-  def self.get_apk_path(flavor:, fastfile_dir:)
-    flutter_root = File.expand_path('../..', fastfile_dir)
-    base_path = File.join(flutter_root, 'build', 'app', 'outputs', 'apk', 'release')
-
-    if flavor.to_s.strip.empty?
-      File.join(base_path, 'app-release.apk')
-    else
-      File.join(base_path, "app-#{flavor}-release.apk")
-    end
-  end
-
-  def self.get_env_key(base_key, flavor)
-    if flavor.to_s.empty?
-      base_key
-    else
-      "#{base_key}_#{flavor.upcase}"
-    end
+  def self.get_apk_path(relative_path:, fastfile_dir:)
+    root = flutter_root(fastfile_dir: fastfile_dir)
+    File.join(root, 'build', 'app', 'outputs', relative_path)
   end
 end
---
+++ ios/fastlane/README.md
@@ -0,0 +1,48 @@
+fastlane documentation
+----
+
+# Installation
+
+Make sure you have the latest version of the Xcode command line tools installed:
+
+```sh
+xcode-select --install
+```
+
+For _fastlane_ installation instructions, see [Installing _fastlane_](https://docs.fastlane.tools/#installing-fastlane)
+
+# Available Actions
+
+## iOS
+
+### ios flutter_clean
+
+```sh
+[bundle exec] fastlane ios flutter_clean
+```
+
+
+
+### ios build_ios_no_codesign
+
+```sh
+[bundle exec] fastlane ios build_ios_no_codesign
+```
+
+
+
+### ios distribute_to_firebase
+
+```sh
+[bundle exec] fastlane ios distribute_to_firebase
+```
+
+
+
+----
+
+This README.md is auto-generated and will be re-generated every time [_fastlane_](https://fastlane.tools) is run.
+
+More information about _fastlane_ can be found on [fastlane.tools](https://fastlane.tools).
+
+The documentation of _fastlane_ can be found on [docs.fastlane.tools](https://docs.fastlane.tools).
